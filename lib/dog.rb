a28a48988c37082b4b6c3662e45bd9366fed4cc8
sopharsogood <justanothersophie@gmail.com>
2021-07-22 21:18:07 -0400

    commit tree 73169fc6ef987d138f3717b63061b6adf92923e7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+# What if instead of a puppy lab, this were a puppy newfie?
+# ...I'm sorry
+
+class Dog
+    
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        self.name = name
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        self.all.each do |doggy|
+            puts doggy.name
+        end
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def save
+        @@all << self
+    end
+end
